0df5dbc9e370232c30fb77f4c635365e1d6e188e
pocketcorp <pocketcorp@protonmail.com>
2023-12-22 05:42:46 -0800

    header complete

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -7,7 +7,6 @@ import { Gutter } from '../../Gutter'
 import Link from 'next/link'
 import Image from 'next/image'
 import { HeaderNav } from '../Nav'
-import MobileNav from '../MobileNav'
 
 import classes from './index.module.scss'
 import { noHeaderFooterUrls } from '../../../constants'
@@ -18,13 +17,12 @@ const HeaderComponent = ({header}: {header: Header}) => {
 
   return (
     <nav className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide].filter(Boolean).join(' ')}>
-        <Gutter className={[classes.wrap]}>
+        <Gutter className={classes.wrap}>
             <Link href="/">
                 <Image src="/logo-black.svg" alt="logo" width={170} height={50}/>
             </Link>
 
             <HeaderNav header={header} />
-            <MobileNav header={header} />
         </Gutter>
     </nav>
   )
