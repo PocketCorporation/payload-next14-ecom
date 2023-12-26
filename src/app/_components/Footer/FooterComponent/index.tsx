fcc9012f81e9ba6a147d35ac0e0193a04b3c3c0b
pocketcorp <pocketcorp@protonmail.com>
2023-12-26 08:39:38 -0800

    footer cont

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -1,9 +1,50 @@
+'use client'
+
 import React from 'react'
 import classes from './index.module.scss'
+import { Gutter } from '../../Gutter'
+import { inclusions, noHeaderFooterUrls } from '../../../constants'
+import { usePathname } from 'next/navigation'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Footer } from '../../../../payload/payload-types'
 
-const FooterComponent = () => {
+const FooterComponent = ({footer}: {footer:Footer}) => {
+  const pathname = usePathname()
+
   return (
-    <div>Footer</div>
+    <footer className={noHeaderFooterUrls.includes(pathname) ? classes.hide : ''}>
+      <Gutter>
+        <ul className={classes.inculsions}>
+          {inclusions.map((inclusions)=>(
+            <li key={inclusions.title}>
+              <Image 
+                src={inclusions.icon}
+                alt={inclusions.title}
+                width={36}
+                height={36}
+                className={classes.icon}
+              />
+
+              <h5 className={classes.title}>{inclusions.title}</h5>
+              <p>{inclusions.description}</p>
+            </li>
+          ))}
+        </ul>
+      </Gutter>
+      
+      <div className={classes.footer}>
+        <Gutter>
+          <div className={classes.wrap}>
+            <Link href='/'>
+              <Image src="/logo-white.svg" alt="logo" width={170} height={50} />
+            </Link>
+
+            <p>{footer.copyright}</p>
+          </div>
+        </Gutter>
+      </div>
+    </footer>
   )
 }
 
