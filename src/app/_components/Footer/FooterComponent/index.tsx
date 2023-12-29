4a8b445b8fa4474e5ef2af85a371c77a7cc33673
pocketcorp <pocketcorp@protonmail.com>
2023-12-29 05:27:22 -0800

    updated globals

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -37,7 +37,7 @@ const FooterComponent = ({footer}: {footer:Footer}) => {
         <Gutter>
           <div className={classes.wrap}>
             <Link href='/'>
-              <Image src="/logo-white.svg" alt="logo" width={170} height={50} />
+              <Image src="/logo-white.svg" alt="logo" width={170} height={50} />pocket
             </Link>
 
             <p>{footer.copyright}</p>
